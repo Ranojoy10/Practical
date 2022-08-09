21af1cc395dbc8cad101b471b222d77c4094a3a1
Ranojoy10 <110466839+Ranojoy10@users.noreply.github.com>
2022-08-09 16:39:15 +0530

    Update A.java

--- A.java
+++ A.java
@@ -1,13 +1,13 @@
 class A
 
 {
-  static int c = 30;
-  int a = 10;
-  public static void main(String...args)
+  static int c = 30; // static variable // with the help of classname dot variable
+  int a = 10;      // Instance variable // obj create object 
+  public static void main(String...args)   
 {
 
-   A obj = new A();
-   int b= 20;
+   A obj = new A(); // creating object
+   int b= 20;       // Local variable // directly
    System.out.println(A.c);
    System.out.println(b);
    System.out.println(obj.a);
