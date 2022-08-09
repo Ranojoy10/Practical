09d866702212fdaee4d917e416800068f7348ed9
Ranojoy10 <110466839+Ranojoy10@users.noreply.github.com>
2022-08-09 16:29:26 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,19 @@
+//add two number
+
+class Addition{
+
+public static void main(String[]args){ // main method
+
+System.out.println("enter two numbers:");  // print the line
+
+int a= 5;
+
+int b= 6;
+
+System.out.println(a+ " " + b);
+
+int sum = a+b;
+
+System.out.println("The sum is:=" +sum);
+}
+}
