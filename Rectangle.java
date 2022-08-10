09793e0a1cd2c46370e1f515dc8dbfc5e8bf42b5
Ranojoy10 <110466839+Ranojoy10@users.noreply.github.com>
2022-08-10 15:35:15 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,17 @@
+//Area and perimeter of the rectangle
+
+class Rectangle{
+
+public static void main(String[]args){ // main method
+
+int l= 5;
+
+int b=6;
+
+int area_Rectangle= l*b ;
+int perimeter_Rectangle= 2*(l+b);
+
+System.out.println("The Area of the Rectangle is:=" +area_Rectangle);
+System.out.println("The Perimeter of the Rectangle is:=" +perimeter_Rectangle);
+}
+}
---
+++ Square.java
@@ -0,0 +1,15 @@
+//Area and perimeter of the square
+
+class Square{
+
+public static void main(String[]args){ // main method
+
+int side= 5;
+
+int area_square= side*side ;
+int perimeter_square= 4*side;
+
+System.out.println("The Area of the square is:=" +area_square);
+System.out.println("The Perimeter of the square is:=" +perimeter_square);
+}
+}
