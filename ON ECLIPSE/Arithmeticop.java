73d74706bf0531c8a3651d0cf1274f14452bedaa
Ranojoy10 <110466839+Ranojoy10@users.noreply.github.com>
2022-08-18 08:38:55 +0530

    Add files via upload

---
+++ ON ECLIPSE/Arithmeticop.java
@@ -0,0 +1,20 @@
+package Operator;
+
+public class Arithmeticop {
+
+	public static void main(String[] args) {
+		
+		// initialize a & b
+		
+		    int a=10;
+		    int b=20;
+		    
+		    System.out.println(a+b); //addition
+		    System.out.println(a-b); //subtraction
+		    System.out.println(a*b); //multiplication
+		    System.out.println(a/b); //Division
+		    System.out.println(a%b); //remainder
+
+	}
+
+}
