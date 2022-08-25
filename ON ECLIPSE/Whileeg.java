5cc9a2b2db9e0ac41602f7721a9c46dbd0b4b81c
Ranojoy10 <110466839+Ranojoy10@users.noreply.github.com>
2022-08-25 17:34:38 +0530

    Add files via upload

---
+++ ON ECLIPSE/Whileeg.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class Whileeg {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=5) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
---
+++ ON ECLIPSE/Whileloop.java
@@ -0,0 +1,15 @@
+package Operator;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		int i=0;
+		System.out.println("First 10 even numbers \n");
+		do {
+			System.out.println(i);
+			i=i+2;
+		}while(i<=10);
+
+	}
+
+}
