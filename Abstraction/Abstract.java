614cfca0155585eb8091bc53772e19c0f64519d3
Ranojoy10 <110466839+Ranojoy10@users.noreply.github.com>
2022-09-21 12:13:57 +0530

    Add files via upload

---
+++ Abstraction/Abstract.java
@@ -0,0 +1,20 @@
+package Operator;
+
+public abstract class Abstract {//parent class
+abstract void method();// abstract method
+}
+class Test11 extends Abstract{//child class 
+	void method() // non abstract method
+	{
+		System.out.println(" method running successfully");
+	}
+}
+class NewTest extends Abstract{//child class 2
+	void method() {
+		System.out.println(" new test method running successfully");
+	}
+	public static void main(String[] args) {
+		Abstract abs=new NewTest();
+		abs.method();
+	}
+}
